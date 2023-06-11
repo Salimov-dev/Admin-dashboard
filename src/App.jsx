0efb35b80ebb7b9337e3d27e872b17bac35498f3
Salimov-dev <salimov.rent@mail.ru>
2023-06-11 21:18:03 +0300

    добавил таблицы на страницы

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
 import { ProSidebarProvider } from "react-pro-sidebar";
 import Team from "./scenes/team";
+import Contacts from "./scenes/contacts";
+import Invoices from "./scenes/invoices";
 // import Invoices from "./scenes/invoices";
 // import Contacts from "./scenes/contacts";
 // import Bar from "./scenes/bar";
@@ -31,8 +33,8 @@ function App() {
               <Routes>
                 <Route path="/" element={<Dashboard />} />
                 <Route path="/team" element={<Team />} />
-                {/* <Route path="/contacts" element={<Contacts />} /> */}
-                {/* <Route path="/invoices" element={<Invoices />} /> */}
+                <Route path="/contacts" element={<Contacts />} />
+                <Route path="/invoices" element={<Invoices />} />
                 {/* <Route path="/form" element={<Form />} /> */}
                 {/* <Route path="/bar" element={<Bar />} /> */}
                 {/* <Route path="/pie" element={<Pie />} /> */}
